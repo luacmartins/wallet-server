6ac0dc46ffe962b8ba73ca544d8b292c839f725f
Carlos Martins <luacmartins@gmail.com>
2020-12-17 17:38:14 -0700

    add edit transaction capabilities

--- routers/transactions.js
+++ routers/transactions.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const Transaction = require('../models/transaction')
 
-
 router.get('/api/transactions', async (req, res) => {
    try {
       const transactions = await Transaction.find({ owner: req.user._id })
@@ -11,6 +10,16 @@ router.get('/api/transactions', async (req, res) => {
          else data['posted'].push(item)
       })
       res.status(200).send(data)
+   } catch (error) {
+      res.status(500).send()
+   }
+})
+
+router.patch('/api/transactions/:id', async (req, res) => {
+   try {
+      const id = req.params.id
+      await Transaction.findOneAndUpdate({ owner: req.user._id, _id: id }, req.body)
+      res.status(200).send()
    } catch (error) {
       res.status(500).send()
    }
