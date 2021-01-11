41f0b3df828ed346d84395c5982b00f4881c4255
Carlos Martins <luacmartins@gmail.com>
2021-01-11 15:41:04 -0700

    fix previous commit to pass in right arguments

--- utils/getDashboardTransactions.js
+++ utils/getDashboardTransactions.js
@@ -1,4 +1,4 @@
-const getDashboardTransactions = (owner, transactions) => {
+const getDashboardTransactions = (transactions) => {
    const max = transactions.slice(0, 10)
    const pending = max.filter(item => item.pending).slice(0, 5)
    const posted = max.filter(item => !item.pending).slice(0, 5)
