4eacf282d17e02a1e26e8e101d27e11ab61a473a
Carlos Martins <luacmartins@gmail.com>
2021-01-15 16:27:37 -0700

    add fetch partial trends

--- routers/trends.js
+++ routers/trends.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 
 router.get('/api/trends', async (req, res) => {
    try {
+      const data = req.user.trends
+      data.monthly = data.monthly[req.query.monthlyPeriod]
+      data.overtime = data.overtime[req.query.overtimePeriod]
       res.status(200).send(req.user.trends)
    } catch (error) {
       console.log(error)
