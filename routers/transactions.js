8de2b8e808b11e1415e01600e709c6557089c234
Carlos Martins <luacmartins@gmail.com>
2021-04-01 09:26:39 -0600

    fix transaction update route

--- routers/transactions.js
+++ routers/transactions.js
@@ -2,45 +2,52 @@ const router = require('express').Router()
 const Transaction = require('../models/transaction')
 const applyFilter = require('../utils/applyFilter')
 const getFilterList = require('../utils/getFilterList')
+const getDashboard = require('../utils/getDashboard')
+const getTrends = require('../utils/getTrends')
 
 router.get('/api/transactions/', async (req, res) => {
-   try {
-      const filter = applyFilter(req.query)
-      const transactions = await Transaction.paginate({ owner: req.user._id, ...filter }, { limit: 30, page: req.query.page, sort: { 'date.user': -1 } })
+  try {
+    const filter = applyFilter(req.query)
+    const transactions = await Transaction.paginate(
+      { owner: req.user._id, ...filter },
+      { limit: 30, page: req.query.page, sort: { 'date.user': -1 } }
+    )
 
-      const data = { 'pending': [], 'posted': [] }
-      transactions.docs.forEach(item => {
-         if (item.pending) data['pending'].push(item)
-         else data['posted'].push(item)
-      })
+    const data = { pending: [], posted: [] }
+    transactions.docs.forEach(item => {
+      if (item.pending) data['pending'].push(item)
+      else data['posted'].push(item)
+    })
 
-      res.set('x-total-pages', transactions.totalPages)
-      res.set('Access-Control-Expose-Headers', 'x-total-pages')
-      res.status(200).send(data)
-   } catch (error) {
-      res.status(500).send()
-   }
+    res.set('x-total-pages', transactions.totalPages)
+    res.set('Access-Control-Expose-Headers', 'x-total-pages')
+    res.status(200).send(data)
+  } catch (error) {
+    res.status(500).send()
+  }
 })
 
 router.patch('/api/transactions/:id', async (req, res) => {
-   try {
-      const id = req.params.id
-      await Transaction.findOneAndUpdate({ owner: req.user._id, _id: id }, req.body)
-      res.status(200).send()
-   } catch (error) {
-      res.status(500).send()
-   }
+  try {
+    const id = req.params.id
+    const transaction = await Transaction.findOneAndUpdate(
+      { owner: req.user._id, _id: id },
+      req.body
+    )
+    await getDashboard(transaction.item)
+    await getTrends(transaction.item)
+    res.status(200).send()
+  } catch (error) {
+    res.status(500).send()
+  }
 })
 
 // get filters
 router.get('/api/filters', async (req, res) => {
-   try {
-      const list = await getFilterList(req.user._id)
-      res.status(200).send(list)
-   } catch (error) {
-
-   }
+  try {
+    const list = await getFilterList(req.user._id)
+    res.status(200).send(list)
+  } catch (error) {}
 })
-
 
 module.exports = router
--- utils/getDashboard.js
+++ utils/getDashboard.js
@@ -9,35 +9,36 @@ const getDashboardTransactions = require('../utils/getDashboardTransactions')
 const getNetWorth = require('../utils/getNetWorth')
 const getPeriod = require('../utils/getPeriod')
 
-const getDashboard = async (itemId) => {
-   try {
-      // Accounts summary
-      const item = await Item.findOne({ itemId })
-      const user = await User.findOne({ _id: item.owner })
-      const accounts = await Account.find({ owner: user._id })
-      const transactions = await Transaction.find({ owner: user._id }, null, { sort: { 'date.user': -1 } })
-      const accountsSummary = getAccountTypeSummary(accounts)
+const getDashboard = async itemId => {
+  try {
+    // Accounts summary
+    const item = await Item.findOne({ $or: [{ itemId }, { _id: itemId }] })
+    const user = await User.findOne({ _id: item.owner })
+    const accounts = await Account.find({ owner: user._id })
+    const transactions = await Transaction.find({ owner: user._id }, null, {
+      sort: { 'date.user': -1 },
+    })
+    const accountsSummary = getAccountTypeSummary(accounts)
 
-      // Latest transactions
-      const dashboardTransactions = getDashboardTransactions(transactions)
+    // Latest transactions
+    const dashboardTransactions = getDashboardTransactions(transactions)
 
-      // Net Worth
-      const { numDays, allTimeDate } = getPeriod(transactions)
-      const networth = await getNetWorth(accounts, transactions, numDays)
+    // Net Worth
+    const { numDays, allTimeDate } = getPeriod(transactions)
+    const networth = await getNetWorth(accounts, transactions, numDays)
 
-      const data = {
-         accounts: accountsSummary,
-         transactions: dashboardTransactions,
-         networth,
-         allTimeDate
-      }
+    const data = {
+      accounts: accountsSummary,
+      transactions: dashboardTransactions,
+      networth,
+      allTimeDate,
+    }
 
-      user.dashboard = data
-      await user.save()
-
-   } catch (error) {
-      console.log(error)
-   }
+    user.dashboard = data
+    await user.save()
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = getDashboard
